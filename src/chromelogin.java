f1f27812040c154453e67d8acbef5fb3d9c20496
rubinshteyn89 <rubinshteyn89@gmail.com>
2017-12-14 11:47:30 -0800

    Update chromelogin.java

--- src/chromelogin.java
+++ src/chromelogin.java
@@ -24,7 +24,7 @@ public class chromelogin {
 		driver.findElement(By.id("gb_70")).click();
 		driver.findElement(By.id("identifierId")).sendKeys(LOGINID);
 		driver.findElement(By.id("identifierNext")).click();
-		driver.findElement(By.name("password")).sendKeys(PASS);
+		driver.findElement(By.Xpath("//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(PASS);
 		driver.findElement(By.id("passwordNext")).click();
 		driver.close();
 	}
