fbf167a6f6f1ec76e51cfeeeef0a37c950a8eaf7
Nikita Karayev <n.karayev@sisa.samsung.com>
2017-12-14 11:25:15 -0800

    Initial Commit

---
+++ src/chromelogin.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chromelogin {
+	
+	private String LOGINID = "***"; //Enter Valid Login
+	private String PASS = "***"; //Enter Valid Pass
+
+	
+	@BeforeClass
+	public static void preSet() {
+	}
+	
+	@Test
+	public void testValidLogin() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\n.karayev\\Documents\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		driver.findElement(By.id("gb_70")).click();
+		driver.findElement(By.id("identifierId")).sendKeys(LOGINID);
+		driver.findElement(By.id("identifierNext")).click();
+		driver.findElement(By.name("password"))..sendKeys(PASS)
+		driver.findElement(By.id("passwordNext")).click();
+		driver.close();
+	}
+
+}
